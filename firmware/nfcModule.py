e76db2702639a11b1ed3c4c2c13dcb5e1948d69f
Adi Gupta <bobdobchob@gmail.com>
2025-04-06 10:29:08 -0400

    Update nfcModule.py

--- firmware/nfcModule.py
+++ firmware/nfcModule.py
@@ -7,6 +7,6 @@ pn532 = PN532_I2C(i2c, debug=False)
 
 version = pn532.firmware_version
 if version:
-    print(f"Found PN532 with firmware version: {hex(version)}")
+    print(f"Found PN532 with firmware version: {version[0]}.{version[1]}")
 else:
     print("Failed to detect PN532. Check wiring and connections.")
