5da2506dde4bd38b7290b7830f76ac7d12ec285d
AdityaGupta0 <bobdobchob@gmail.com>
2025-04-17 02:14:01 -0400

    Update loadCell.py

--- firmware/loadCell.py
+++ firmware/loadCell.py
@@ -3,8 +3,8 @@ from hx711 import HX711  # Ensure you have the HX711 library installed
 import RPi.GPIO as GPIO  # Ensure you have the RPi.GPIO library installed
 
 # Define GPIO pins for the HX711
-DT = 5  # Data pin (DT)
-SCK = 6  # Clock pin (SCK)
+DT = 21  # Data pin (DT)
+SCK = 20  # Clock pin (SCK)
 
 
 
